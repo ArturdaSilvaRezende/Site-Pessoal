bc5059baab41d6adf6e2117294bfd73e9a6d0cea
Artur da Silva Rezende <artursrezende@gmail.com>
2020-08-15 22:22:32 -0300

    Descrição das skills área, finalizado e responsivo

--- src/js/rotas.js
+++ src/js/rotas.js
@@ -11,3 +11,15 @@ import '../assets/close.png'; //close menu
 
 //About imgage
 import '../assets/artur-rezende.jpg'; //Artur img about
+
+//Skills front end 
+import '../assets/html5.png';
+import '../assets/css3.png';
+import '../assets/javascript.png';
+import '../assets/bootstrap.png';
+import '../assets/jquery.png';
+import '../assets/sass.png';
+import '../assets/webpack.png';
+import '../assets/php.png';
+import '../assets/mysql.png';
+import '../assets/git.png';
