2dc006fe1aad3685127b87fd39f9479c73b5db19
Artur da Silva Rezende <arturrezende4@gmail.com>
2021-01-29 10:48:27 -0300

    About content melhorado e centralizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,14 @@
 /**
- * Projeto Site Pessoal Versão 1.1
+ * Projeto Site Pessoal Versão 1.1.2
  * @author Artur da Silva Rezende
  */
 
-'use strict';
+"use strict";
 
-import { rotas } from './rotas'
-import { menuNavegacao } from './menu_main'
-import { fullDataTime } from './full_data_time'
-import { tipeWrite } from './tipewrite'
-import { javaMessage } from './java_skill_control'
-import { portfolioContent } from './portfolio'
-import { navegacaoLinks } from './navegacao-link'
-
-
-
-
+import { rotas } from "./rotas";
+import { menuNavegacao } from "./menu_main";
+import { fullDataTime } from "./full_data_time";
+import { tipeWrite } from "./tipewrite";
+import { javaMessage } from "./java_skill_control";
+import { portfolioContent } from "./portfolio";
+import { navegacaoLinks } from "./navegacao-link";
