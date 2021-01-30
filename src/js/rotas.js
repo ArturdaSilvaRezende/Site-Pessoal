2a43163fe6d5b5ec2c14564306bba2a057175823
Artur da Silva Rezende <arturrezende4@gmail.com>
2021-01-30 14:29:17 -0300

    Área skills melhorada, acrescentado a skill gulp

--- src/js/rotas.js
+++ src/js/rotas.js
@@ -1,43 +1,44 @@
 //custom style
-import '../css/style.scss';
-import '../css/reset.scss';
-import '../css/mixin-variaveis.scss';
-import '../css/container-geral.scss';
-import '../css/header.scss';
-import '../css/main-content.scss';
-import '../css/portfolio.scss';
-import '../css/area-contato.scss';
+import "../css/style.scss";
+import "../css/reset.scss";
+import "../css/mixin-variaveis.scss";
+import "../css/container-geral.scss";
+import "../css/header.scss";
+import "../css/main-content.scss";
+import "../css/portfolio.scss";
+import "../css/area-contato.scss";
 
 //header image
-import '../assets/favicon.png'; //favicon
-import '../assets/logo.png'; //logo
-import '../assets/open-menu.png'; //open menu
-import '../assets/close.png'; //close menu
+import "../assets/favicon.png"; //favicon
+import "../assets/logo.png"; //logo
+import "../assets/open-menu.png"; //open menu
+import "../assets/close.png"; //close menu
 
 //About imgage
-import '../assets/artur-rezende.jpg'; //Artur img about
+import "../assets/artur-rezende.jpg"; //Artur img about
 
-//Skills front end 
-import '../assets/html5.png';
-import '../assets/css3.png';
-import '../assets/javascript.png';
-import '../assets/bootstrap.png';
-import '../assets/jquery.png';
-import '../assets/sass.png';
-import '../assets/webpack.png';
-import '../assets/php.png';
-import '../assets/mysql.png';
-import '../assets/git.png';
-import '../assets/java.png';
+//Skills front end
+import "../assets/html5.png";
+import "../assets/css3.png";
+import "../assets/javascript.png";
+import "../assets/bootstrap.png";
+import "../assets/jquery.png";
+import "../assets/sass.png";
+import "../assets/webpack.png";
+import "../assets/gulp.png";
+import "../assets/php.png";
+import "../assets/mysql.png";
+import "../assets/git.png";
+import "../assets/java.png";
 
 //Portifólio
-import '../assets/portfolio-01.jpg';
-import '../assets/portfolio-02.jpg';
-import '../assets/portfolio-03.jpg';
-import '../assets/portfolio-04.jpg';
+import "../assets/portfolio-01.jpg";
+import "../assets/portfolio-02.jpg";
+import "../assets/portfolio-03.jpg";
+import "../assets/portfolio-04.jpg";
 
 //Contanto
-import '../assets/gmail.png';
-import '../assets/linkedin.png';
-import '../assets/github.png';
-import '../assets/whatsapp.png';
+import "../assets/gmail.png";
+import "../assets/linkedin.png";
+import "../assets/github.png";
+import "../assets/whatsapp.png";
